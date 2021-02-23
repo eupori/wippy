73faf270ea7010c6bd1b536d60e5d082eed341e8
이채민 <cmlee-intern@insilicogen.com>
2021-02-23 21:29:42 +0900

    회원 모델 수정

--- nrise_cmlee/nrise_app/models.py
+++ nrise_cmlee/nrise_app/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(User):
-	lastest_logout = models.DateTimeField(null=True, blank=True),
-	created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True),
+class User(models.Model):
+	user_id = models.CharField(max_length=100)
+	password = models.CharField(max_length=64, unique=True)
+	lastest_logout = models.DateTimeField(null=True, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+	is_active = models.BooleanField(default=True)
 
 
 class Session(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="user_id"),
-	ip_address = models.CharField(max_length=100, null=True, blank=True),
-	lastest_logout = models.DateTimeField(null=True, blank=True),
-	created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True),
+	user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="user_id")
+	ip_address = models.CharField(max_length=100, null=True, blank=True)
+	lastest_logout = models.DateTimeField(null=True, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
--- nrise_cmlee/nrise_app/views.py
+++ nrise_cmlee/nrise_app/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import permissions
 from django.http import JsonResponse
+from nrise_app.models import (
+	User,
+	Session,
+)
+
+import hashlib
 
 # Create your views here.
 
@@ -9,4 +15,35 @@ class UserAPIView(APIView):
 	permission_classes=(permissions.AllowAny,)
 	
 	def post(self, request, *args, **kwargs):
+		user_id = request.POST.get("user_id")
+		password = request.POST.get("password")
+
+		#비밀번호 해싱
+		hash_pwd = hashlib.sha256(password.encode()) 
+
+		#사용자 생성
+		User.objects.create(user_id=user_id, password=hash_pwd.hexdigest())
+
+		return JsonResponse({"data":"data"},status=200)
+
+
+class UserLoginAPIView(APIView):
+	permission_classes=(permissions.AllowAny,)
+
+	def post(self, request, *args, **kwargs):
+		user_id = request.POST.get("user_id")
+		password = request.POST.get("password")
+		login_status = False
+
+		#비밀번호 해싱
+		hash_pwd = hashlib.sha256(password.encode()) 
+
+		#사용자 생성
+		user = User.objects.get(user_id=user_id)
+
+		#사용자 비밀번호 확인
+		if user.password.equals(hash_pwd.hexdigest()):
+			login_status = True
+			Session.objects.create()
+
 		return JsonResponse({"data":"data"},status=200)
